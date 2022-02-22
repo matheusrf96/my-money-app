9eaab593a3bdd136d3c201d6785fbdfaebac1c08
Matheus Rodrigues <matheusrf96@gmail.com>
2022-02-22 18:00:57 -0300

    Implement cors and express-query-int

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,11 +1,15 @@
 const port = 3003
 
 const bodyParser = require('body-parser')
+const queryParser = require('express-query-int')
 const express = require('express')
+const allowCors = require('./cors')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(queryParser)
 
 server.listen(port, () => {
     console.log(`BACKEND running on port ${ port }.`)
