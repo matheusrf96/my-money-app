5ebbfea9660e040404451e88fcd35914bb898367
Matheus Rodrigues <matheusrf96@gmail.com>
2022-02-22 16:09:54 -0300

    Implement summary route

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -5,21 +5,38 @@ BillingCycle.updateOptions({ new: true, runValidators: true })
 
 BillingCycle.route('get', (req, res, next) => {
     BillingCycle.find({}, (err, docs) => {
-        if (!err) {
-            res.json(docs)
-        } else {
+        if (err) {
             res.status(500).json({ errors: [err] })
+        } else {
+            res.json(docs)
         }
     })
 })
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((err, val) => {
-        if (!err) {
-            res.json({ value: val })
+        if (err) {
+            res.status(500).json({ errors: [err] })
         }
         else {
+            res.json({ value: val })
+        }
+    })
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([{
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" }},
+    }, {
+        $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" }},
+    }, {
+        $project: { _id: 0, credit: 1, debt: 1 },
+    }], (err, result) => {
+        if (err) {
             res.status(500).json({ errors: [err] })
+        }
+        else {
+            res.json(result[0] || { credit: 0, debt: 0 })
         }
     })
 })
